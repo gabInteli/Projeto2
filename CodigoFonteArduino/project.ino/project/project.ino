914f8382b9277f9f4ee57f4bb43967f1ed8de12a
0rdep12 <78965293+0rdep12@users.noreply.github.com>
2022-11-15 15:03:51 -0300

    update

---
+++ CodigoFonteArduino/project.ino/project/project.ino
@@ -0,0 +1,34 @@
+// Inicialização de variáveis globais, comportarão saídas de LED's e o valor que elas guardam é referente aos pinos que os alimentam
+int blueTemp = 21;//definição da pinagem do led azul do RGB de temperatura        
+int greenTemp = 20;//definição de pinagem do led verde do RGB de temperatura       
+int redTemp = 19;//definição de pinagem do led vermelho do RGB de temperatura    
+int blueHum = 39;//definiçao de pinagem do led azul do RGB de umidade       
+int greenHum = 40;//definição de pinagem do led verde do RGB de umidade      
+int redHum = 41;//definição de pinagem do led vermelho 
+
+float value1;//variavel criada salvar o valor da temperatura  
+float value2;//variavel criada salvar o valor da umidade 
+float value3;//dnão esta sendo utilizado no momento 
+
+void setup() {
+    wifiInitialization(); // Funçaõ que inicializa a conexão wi-fi do microcontrolador
+    sensorInitialization(); // Função que inicializa a leitura do sensor de temperatura e umidade
+    displayInitialization(); // Função que inicializa o Display LCD
+    leds();
+}
+
+
+void loop() {
+  delay(500);//Intervalo para reiniciar loop
+  float temp_01 = scanTemperature(); // Variavel que pega os valores de temperatura do ambiente
+  float humidity_01 = scanHumidity(); // Variavel que pega os valores de umidade do ambiente
+  showMessage(temp_01, humidity_01); // Função que indica no LCD os valores lidos de temperatura e umidade
+  color_led();
+  value1 = temp_01;
+  value2 = humidity_01;
+  sendDataToSheet();
+  delay(10000);
+}
+
+
+
---
+++ CodigoFonteArduino/project.ino/project/sensor.ino
@@ -0,0 +1,90 @@
+#include <Wire.h>
+#include <Adafruit_AHTX0.h>
+
+
+Adafruit_AHTX0 aht; // Cria um objeto para o sensor(biblioteca do AHT10)
+
+//define os parametros de OUTPUT para os pinos do RGB
+void leds(){
+    pinMode(blueTemp, OUTPUT);       
+    pinMode(greenTemp, OUTPUT);      
+    pinMode(redTemp, OUTPUT);   
+    pinMode(blueHum, OUTPUT);      
+    pinMode(greenHum, OUTPUT);     
+    pinMode(redHum, OUTPUT);
+}
+
+// Inicialização do sensor
+void sensorInitialization() {
+  Wire.begin(SDA_PIN, SCL_PIN); // Configuração dos pinos do sistema
+  // Checa se o sensor foi identificado pelo sistema ou não
+  Serial.begin(115200);
+  Serial.println("Adafruit AHT10/AHT20 demo!");
+  if (! aht.begin()) {
+    Serial.println("Could not find AHT? Check wiring");
+    while (1) delay(10);
+  }
+  Serial.println("AHT10 or AHT20 found");
+}
+
+// Função que retorna a temperatura do ambiente
+float scanTemperature() {
+  sensors_event_t humidity, temp; // Criação de objetos
+  aht.getEvent(&humidity, &temp); // Preenche os objetos temp e humidity com dados novos.
+  Serial.print("Temperatura: "); Serial.print(temp.temperature); Serial.println(" degrees C"); 
+}
+
+//Função que retorna a umidade do ambiente
+float scanHumidity() {
+  sensors_event_t humidity, temp; // Criação de objetos
+  aht.getEvent(&humidity, &temp); // Preenche os objetos temp e humidity com dados novos.
+  Serial.print("Umidade: "); Serial.print(humidity.relative_humidity); Serial.println("% rH");
+}
+
+void color_led(){
+    sensors_event_t humidity, temp; // Criação de objetos
+    aht.getEvent(&humidity, &temp); // Preenche os objetos temp e humidity com dados novos.
+      // Verifica as temperaturas obtidas e acende o led correspondente de acordo com o resultado obtido.    
+      if (temp.temperature > 23 && temp.temperature < 30) {
+        rightTemperature();
+          delay(10000);
+      }
+      else {
+        wrongTemperature();
+          delay(10000);
+    }
+      if (humidity.relative_humidity < 80 && humidity.relative_humidity > 40) {
+      rightHumidity();
+        delay(10000);
+    }
+    else {
+      wrongHumidity();
+        delay(10000);
+    }
+}
+
+//Funções para fazer o led rgb ficar nas cores desejadas, de acordo com o ambiente.
+// Luz que refere à temperatura quando não está em seu estado ideal.
+void wrongTemperature() {
+            digitalWrite(redTemp, HIGH);  //aciona a luz vermelha do led de temperatura.
+            digitalWrite(greenTemp, LOW); //desativa luz verde do led de temperatura.
+            delay(1000);        
+        }
+// Luz que refere à temperatura quando está em seu estado ideal.
+void rightTemperature() {
+            digitalWrite(greenTemp, HIGH); //aciona luz verde do led de temperatura.
+            digitalWrite(redTemp, LOW); //desativa luz vermelha do led de temperatura.
+            delay(1000);
+        }
+// Luz que refere à umidade quando não está em seu estado ideal.
+void wrongHumidity() {
+            digitalWrite(redHum, HIGH);  //aciona luz vermelha do led de umidade.
+            digitalWrite(greenHum, LOW); //desativa luz verde do led de umidade.
+            delay(1000);        
+        }
+// Luz que refere à umidade quando está em seu estado ideal.
+void rightHumidity() {
+            digitalWrite(greenHum, HIGH); //aciona luz verde do led de umidade.
+            digitalWrite(redHum, LOW); //desativa luz vermelha do led de umidade.
+            delay(1000);
+        }
