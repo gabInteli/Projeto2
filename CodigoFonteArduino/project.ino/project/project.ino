9b8c5b0d576655f3276999868a55a314ad4806f8
emanuel-inteli <82065728+emanuelcop3@users.noreply.github.com>
2022-11-16 14:42:39 -0300

    encapsulamento

--- CodigoFonteArduino/project.ino/project/project.ino
+++ CodigoFonteArduino/project.ino/project/project.ino
@@ -1,14 +1,13 @@
 // Inicialização de variáveis globais, comportarão saídas de LED's e o valor que elas guardam é referente aos pinos que os alimentam
-int blueTemp = 21;//definição da pinagem do led azul do RGB de temperatura        
+const blueTemp = 21;//definição da pinagem do led azul do RGB de temperatura        
 int greenTemp = 20;//definição de pinagem do led verde do RGB de temperatura       
-int redTemp = 19;//definição de pinagem do led vermelho do RGB de temperatura    
-int blueHum = 39;//definiçao de pinagem do led azul do RGB de umidade       
+const redTemp = 19;//definição de pinagem do led vermelho do RGB de temperatura    
+const blueHum = 39;//definiçao de pinagem do led azul do RGB de umidade       
 int greenHum = 40;//definição de pinagem do led verde do RGB de umidade      
-int redHum = 41;//definição de pinagem do led vermelho 
+const redHum = 41;//definição de pinagem do led vermelho 
 
-float value1;//variavel criada salvar o valor da temperatura  
-float value2;//variavel criada salvar o valor da umidade 
-float value3;//dnão esta sendo utilizado no momento 
+float temperatura1;//variavel criada salvar o valor da temperatura  
+float umidade2;//variavel criada salvar o valor da umidade 
 
 void setup() {
     wifiInitialization(); // Funçaõ que inicializa a conexão wi-fi do microcontrolador
@@ -24,8 +23,8 @@ void loop() {
   float humidity_01 = scanHumidity(); // Variavel que pega os valores de umidade do ambiente
   showMessage(String(temp_01), String(humidity_01)); // Função que indica no LCD os valores lidos de temperatura e umidade
   color_led();
-  value1 = temp_01;
-  value2 = humidity_01;
+  temperatura1 = temp_01;
+  umidade2 = humidity_01;
   sendDataToSheet();
   delay(10000);
 }
