8aadd28e1bf165593b2f5431ff15a11a8f9d0c5e
0rdep12 <78965293+0rdep12@users.noreply.github.com>
2022-11-17 14:37:16 -0300

    up

--- CodigoFonteArduino/project.ino/project/sensor.ino
+++ CodigoFonteArduino/project.ino/project/sensor.ino
@@ -48,51 +48,25 @@ void color_led(){
     sensors_event_t humidity, temp; // Criação de objetos
     aht.getEvent(&humidity, &temp); // Preenche os objetos temp e humidity com dados novos.
       // Verifica as temperaturas obtidas e acende o led correspondente de acordo com o resultado obtido.    
-      if (temp.temperature > 23 && temp.temperature < 30) {
-        rightTemperature();
+      if (temp.temperature > 22 && temp.temperature < 30) {
+        digitalWrite(greenTemp, HIGH); //aciona luz verde do led de temperatura.
+        digitalWrite(redTemp, LOW);
           delay(10000);
       }
       else {
-        wrongTemperature();
+        digitalWrite(redTemp, HIGH);  //aciona a luz vermelha do led de temperatura.
+        digitalWrite(greenTemp, LOW);
           delay(10000);
     }
       if (humidity.relative_humidity < 80 && humidity.relative_humidity > 40) {
-      rightHumidity();
+      digitalWrite(greenHum, HIGH); //aciona luz verde do led de umidade.
+      digitalWrite(redHum, LOW);
         delay(10000);
     }
     else {
-      wrongHumidity();
+      digitalWrite(redHum, HIGH);  //aciona luz vermelha do led de umidade.
+      digitalWrite(greenHum, LOW);
         delay(10000);
     }
   return;
 }
-
-//Funções para fazer o led rgb ficar nas cores desejadas, de acordo com o ambiente.
-// Luz que refere à temperatura quando não está em seu estado ideal.
-void wrongTemperature() {
-            digitalWrite(redTemp, HIGH);  //aciona a luz vermelha do led de temperatura.
-            digitalWrite(greenTemp, LOW); //desativa luz verde do led de temperatura.
-            delay(1000);  
-          return;      
-        }
-// Luz que refere à temperatura quando está em seu estado ideal.
-void rightTemperature() {
-            digitalWrite(greenTemp, HIGH); //aciona luz verde do led de temperatura.
-            digitalWrite(redTemp, LOW); //desativa luz vermelha do led de temperatura.
-            delay(1000);
-          return;
-        }
-// Luz que refere à umidade quando não está em seu estado ideal.
-void wrongHumidity() {
-            digitalWrite(redHum, HIGH);  //aciona luz vermelha do led de umidade.
-            digitalWrite(greenHum, LOW); //desativa luz verde do led de umidade.
-            delay(1000);      
-          return;  
-        }
-// Luz que refere à umidade quando está em seu estado ideal.
-void rightHumidity() {
-            digitalWrite(greenHum, HIGH); //aciona luz verde do led de umidade.
-            digitalWrite(redHum, LOW); //desativa luz vermelha do led de umidade.
-            delay(1000);
-          return;
-        }
